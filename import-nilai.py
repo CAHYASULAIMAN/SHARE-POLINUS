0344011c398dbddd7659da50aeb60877cb3587eb
CAHYASULAIMAN <96614622+CAHYASULAIMAN@users.noreply.github.com>
2022-11-03 20:47:28 +0700

    paste pertama uts algoritma

---
+++ import-nilai.py
@@ -0,0 +1,27 @@
+# BATAS INDIKATOR NILAI RAPORT
+print ("========== nilai raport ============")
+print ("")
+# Pengunaan IF ELSE #
+
+nilai = 70
+print ("nilai yang di masukan :",nilai)
+
+
+# KONDISI IF
+if  nilai >= 90       :
+	print ("nilai A")
+
+elif nilai >= 75 <= 89:
+	print ("nilai B")
+
+elif nilai >= 60 <= 74 :
+	print ("nilai C")
+
+elif nilai >= 50 <= 59 :
+	print ("nilai D")
+
+elif nilai >= 0 <   50 :
+	print ("nilai E")
+
+else:
+	print ("nilai tidak valid")

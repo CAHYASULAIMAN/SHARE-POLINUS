39134f130705f4d8a63efdb5353578ada2baefa6
CAHYASULAIMAN <96614622+CAHYASULAIMAN@users.noreply.github.com>
2022-11-03 21:12:39 +0700

    buatan-dengan inputan

--- import-nilai.py
+++ import-nilai.py
@@ -1,27 +1,30 @@
-# BATAS INDIKATOR NILAI RAPORT
-print ("========== nilai raport ============")
+#Bagian pembukaan
+#UTS V.1.0.0
+print ("==========program inputan nilai============")
 print ("")
-# Pengunaan IF ELSE #
+#Bagian perkenalan
+print("NAMA : CAHYA SULAIMAN")
+print("NIM  : B22067")
+print("PRODI: S I C")
+print ("")
+#Bagian penginputan data
+inputan = input('masukan nilai: ')
+#Bagian pengubahan type data
+cvt = int(inputan)
 
-nilai = 70
-print ("nilai yang di masukan :",nilai)
+#Bagian eksekusi data
+if cvt >= 90 and cvt <= 100 :       #eksekusi nilai A 90-100
+    print ("Nilai A")
+elif cvt >= 75 and cvt <= 89 :      #eksekusi nilai B 75-89
+    print("Nilai B")
+elif cvt >= 60 and cvt <= 74 :      #eksekusi nilai C 60-74
+    print("Nilai C")
+elif cvt >= 50 and cvt <= 59 :      #eksekusi nilai D 50-59
+    print("Nilai D")
+elif cvt >= 0 and cvt  <= 49 :      #eksekusi nilai E 0-49
+    print("Nilai E")
+else:                               #eksekusi nilai yang tidak masuk akal atau negatif
+    print("mohon ditulis dengan Angka yang benar")
 
-
-# KONDISI IF
-if  nilai >= 90       :
-	print ("nilai A")
-
-elif nilai >= 75 <= 89:
-	print ("nilai B")
-
-elif nilai >= 60 <= 74 :
-	print ("nilai C")
-
-elif nilai >= 50 <= 59 :
-	print ("nilai D")
-
-elif nilai >= 0 <   50 :
-	print ("nilai E")
-
-else:
-	print ("nilai tidak valid")
+#Bagian penutup
+print ("Selesai")
